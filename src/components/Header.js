fb7a4dd3d72b06662349ef10ca834d3fe040f54c
hamsika <hamsikarao24@gmail.com>
2023-06-06 17:27:05 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className = "ui fixed menu">
+        <div className = "ui container center">
+            <h1>Contact Manager</h1>
+
+        </div>
+
+    </div>
+  );
+};
